d1822375e19ade5c4fa10f101f8fea36541915e2
Wajeeha-Sabir <wajeehasabir75@gmail.com>
2024-12-17 12:13:34 +0500

    TmX_Bank, TmX_Currency & TmX_Location Model created!

---
+++ CorporatePortalApi/Models/TmX_Currency.cs
@@ -0,0 +1,32 @@
+﻿namespace CorporatePortalApi.Models
+{
+    public class TmX_Currency
+    {
+        [Key] // Primary Key
+        public int Currency_ID { get; set; }
+
+        public bool Active_Flag { get; set; }
+        
+        public DateTime Effective_Start_Date { get; set; }
+        public DateTime Effective_End_Date { get; set; }
+
+        [MaxLength(20)]
+        public string? Currency_Code { get; set; }
+
+        [MaxLength(20)]
+        public string Currency_Symbol { get; set; }
+
+        [MaxLength(100)]
+        public string? Currency_Description { get; set; }
+
+        [MaxLength(100)]
+        public string Created_By { get; set; }
+        public DateTime Created_Date { get; set; }
+
+        [MaxLength(100)]
+        public string? Last_Updated_By { get; set; }
+
+        public DateTime? Last_Updated_Date { get; set; }
+
+    }
+}
--- CorporatePortalApi/Models/TmX_Location.cs
+++ CorporatePortalApi/Models/TmX_Location.cs
@@ -2,5 +2,34 @@
 {
     public class TmX_Location
     {
+        [Key] // Primary Key
+        public int Location_ID { get; set; }
+        public int? Parent_Location_ID { get; set; }
+        public virtual TmX_Location Parent_Location{ get; set; }
+        public int Tenant_ID { get; set; }
+        public virtual TMX_Tenant Tenant { get; set; }
+
+        [MaxLength(50)]
+        public string Location_Code { get; set; }
+
+        [MaxLength(200)]
+        public string? Location_Name { get; set; }
+
+        public bool Active_Flag { get; set; }
+
+        public DateTime Effective_Start_Date { get; set; }
+        public DateTime Effective_End_Date { get; set; }
+
+        [MaxLength(100)]
+        public string Created_By { get; set; }
+        public DateTime Created_Date { get; set; }
+
+        [MaxLength(100)]
+        public string? Last_Updated_By { get; set; }
+
+        public DateTime? Last_Updated_Date { get; set; }
+        public int Location_Type_Lkp_ID { get; set; }
+
+        public virtual TmX_Lookup Location_Lookup { get; set; }
     }
 }
