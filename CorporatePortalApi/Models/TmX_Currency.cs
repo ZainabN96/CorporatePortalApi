fc55338e163c54059cd8880304de771c22259eb8
Administrator <Administrator@HCCHRMS>
2024-12-18 10:58:11 +0500

    currency

--- CorporatePortalApi/Models/TmX_Currency.cs
+++ CorporatePortalApi/Models/TmX_Currency.cs
@@ -7,28 +7,35 @@ namespace CorporatePortalApi.Models
         [Key] // Primary Key
         public int Currency_ID { get; set; }
 
+        [Required]
         public bool Active_Flag { get; set; }
-        
+
+        [Required]
         public DateTime Effective_Start_Date { get; set; }
+
+        [Required]
         public DateTime Effective_End_Date { get; set; }
 
         [MaxLength(20)]
         public string? Currency_Code { get; set; }
 
+        [Required]
         [MaxLength(20)]
         public string Currency_Symbol { get; set; }
 
         [MaxLength(100)]
         public string? Currency_Description { get; set; }
 
+        [Required]
         [MaxLength(100)]
         public string Created_By { get; set; }
+
+        [Required]
         public DateTime Created_Date { get; set; }
 
         [MaxLength(100)]
         public string? Last_Updated_By { get; set; }
 
         public DateTime? Last_Updated_Date { get; set; }
-
     }
 }
