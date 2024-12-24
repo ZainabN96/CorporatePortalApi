c2b11b0e706b8235c7352043ffb47ee593904cbe
Wajeeha-Sabir <wajeehasabir75@gmail.com>
2024-12-24 15:07:40 +0500

    Currency Api Created!

---
+++ CorporatePortalApi/Data/IServices/ICurrencyService.cs
@@ -0,0 +1,15 @@
+﻿using CorporatePortalApi.Models;
+
+namespace CorporatePortalApi.Data.IServices
+{
+    public interface ICurrencyService
+    {
+        Task<TmX_Currency> Get(int id);
+        TmX_Currency Add(TmX_Currency TmX_Currency);
+        Task<TmX_Currency> GetWithCurrency_Symbol(string currency);
+        Task<IEnumerable<TmX_Currency>> GetAllCurrencyAsync();
+        Task<bool> IsCurrencyExist(string currency);
+        Task<bool> IsCurrencyExistInUpdate(string currencyS, int id);
+
+	}
+}
---
+++ CorporatePortalApi/Data/Services/CurrencyService.cs
@@ -0,0 +1,49 @@
+﻿using CorporatePortalApi.Data.IServices;
+using CorporatePortalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorporatePortalApi.Data.Services
+{
+    public class CurrencyService: ICurrencyService
+    {
+        private readonly ApplicationDbContext dc;
+
+        public CurrencyService(ApplicationDbContext dc)
+        {
+            this.dc = dc;
+        }
+        public TmX_Currency Add(TmX_Currency TmX_Currency)
+        {
+            dc.TmX_Currency.Add(TmX_Currency);
+
+            return TmX_Currency;
+        }
+
+        public async Task<TmX_Currency> Get(int id)
+        {
+            return await dc.TmX_Currency.FindAsync(id);
+        }
+		public async Task<TmX_Currency> GetWithCurrency_Symbol(string currency)
+		{
+			return await dc.TmX_Currency.Where(x => x.Currency_Symbol == currency)
+									   .FirstOrDefaultAsync();
+		}
+
+		public async Task<IEnumerable<TmX_Currency>> GetAllCurrencyAsync()
+		{
+			return await dc.TmX_Currency.Where(x => x.Active_Flag == true)
+									   .OrderBy(x => x.Currency_Symbol)
+									   .ToListAsync();
+		}
+
+		public async Task<bool> IsCurrencyExist(string currency)
+		{
+			return await dc.TmX_Currency.AnyAsync(x => x.Currency_Symbol == currency);
+		}
+
+		public async Task<bool> IsCurrencyExistInUpdate(string currencyS, int id)
+		{
+			return await dc.TmX_Currency.AnyAsync(x => x.Currency_Symbol == currencyS && x.Currency_ID != id);
+		}
+	}
+}
