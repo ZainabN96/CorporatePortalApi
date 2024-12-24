81ba9835ecf866f3b9f83e6228a97d4d8d7de068
Zainabn096 <zainabnaveed2512@GMAIL.com>
2024-12-24 16:30:05 +0500

    Bank ( Get, Update, Delete)

--- CorporatePortalApi/Controllers/Api/BankController.cs
+++ CorporatePortalApi/Controllers/Api/BankController.cs
@@ -21,11 +21,33 @@ namespace CorporatePortalApi.Controllers.Api
             this.mapper = mapper;
         }
 
+        [HttpGet("get/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var entry = await uow.BankService.Get(id);
+
+            if (entry == null)
+            {
+                APIError apiError = new APIError();
+                apiError.ErrorCode = NoContent().StatusCode;
+                apiError.ErrorMessage = "Bank not found";
+                return BadRequest(apiError);
+            }
+
+            return Ok(entry);
+        }
+
         [HttpGet("getAll")]
         public async Task<IActionResult> GetAll()
         {
             var entries = await uow.BankService.GetAllBankAsync();
-
+            if (entries == null)
+            {
+                APIError apiError = new APIError();
+                apiError.ErrorCode = NoContent().StatusCode;
+                apiError.ErrorMessage = "Bank not found";
+                return BadRequest(apiError);
+            }
             return Ok(entries);
         }
 
@@ -46,6 +68,56 @@ namespace CorporatePortalApi.Controllers.Api
 
             await uow.SaveAsync();
             return Ok(bank);
+        }
+
+        [HttpPut("updateBank")]
+        public async Task<IActionResult> UpdateBank(TmX_BankDto BankDto)
+        {
+            APIError apiError = new APIError();
+
+            if (await uow.BankService.IsBankExistInUpdate(BankDto.Bank_Name, BankDto.Bank_ID))
+            {
+                apiError.ErrorCode = BadRequest().StatusCode;
+                apiError.ErrorMessage = "Bank is already exist";
+                return BadRequest(apiError);
+            }
+
+            var BankFromDb = await uow.BankService.Get(BankDto.Bank_ID);
+
+            if (BankFromDb == null)
+            {
+                apiError.ErrorCode = BadRequest().StatusCode;
+                apiError.ErrorMessage = "Bank not found";
+                return BadRequest(apiError);
+            }
+
+            mapper.Map(BankDto, BankFromDb);
+
+            BankDto.Last_Updated_Date = DateTime.Now;
+
+            await uow.SaveAsync();
+            return Ok(BankDto);
+        }
+
+        [HttpPost("deleteBank")]
+        public async Task<IActionResult> DeleteBank(DeleteKeyPairDto deleteKeyPairDto)
+        {
+            var ProFromDb = await uow.BankService.Get(deleteKeyPairDto.Id);
+
+            if (ProFromDb == null)
+            {
+                APIError apiError = new APIError();
+                apiError.ErrorCode = NoContent().StatusCode;
+                apiError.ErrorMessage = "Bank not found";
+                return BadRequest(apiError);
+            }
+
+            ProFromDb.Is_Active = false;
+            ProFromDb.Last_Updated_Date = DateTime.Now;
+
+            await uow.SaveAsync();
+
+            return Ok(deleteKeyPairDto);
         }
 
     }
